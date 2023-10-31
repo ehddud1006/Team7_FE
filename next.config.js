b83d081ef574e001e644070171a3596456a34e04
ehddud1006 <ehddud1006@naver.com>
2023-10-31 23:27:03 +0900

    배포 테스트

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
   reactStrictMode: true,
-  output: 'standalone',
   images: {
     remotePatterns: [
       {
