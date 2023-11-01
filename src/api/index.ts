d0f47de30761b93d1d53bd073031610eb553d866
ehddud1006 <ehddud1006@naver.com>
2023-11-01 15:37:35 +0900

    배포 테스트

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: `http://3.35.234.197:8080/api/`,
+  baseURL: `http://3.35.234.197:8080/`,
   headers: {
     'Content-Type': 'application/json',
   },
