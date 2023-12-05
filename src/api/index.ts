14955b51b89c33a18931623780d7140d1e8ae9fb
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-12-05 18:53:17 +0900

    deploy: 프론트서버 배포

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://43.201.21.85:8080/api/',
+  baseURL: 'http://3.35.234.197:8080/api/',
   headers: {
     'Content-Type': 'application/json',
   },
